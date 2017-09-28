2faacc8da981b82d76be7e96ed5255950be57694
JasonLee <715480364@qq.com>
2017-09-28 23:32:56 +0800

    2017-09-28

---
+++ BaseCode/pdb_test.py
@@ -0,0 +1,11 @@
+def getAverage(a, b):
+    result = a + b
+    print('Result = %d' % result)
+    return result
+
+a = 100
+b = 200
+c = a + b
+ret = getAverage(a, b)
+
+print('c = %d, red = %d' % (c, ret))

7a5bdf73cd01a07e27903844f397ccbc9dcdff93
JasonLee <715480364@qq.com>
2017-09-30 21:18:12 +0800

    2017-09-30

---
+++ ImproveCode/AlgorithmTest.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# Author: Jason Lee
+import time
+
+# start_time = time.time()
+# for a in range(0, 1001):
+#     for b in range(0, 1001):
+#         for c in range(0, 1001):
+#             if a + b + c == 1000 and a**2 + b**2 == c**2:
+#                 print("a, b, c: %d, %d, %d" % (a, b, c))
+# end_time = time.time()
+
+start_time = time.time()
+for a in range(0, 1001):
+    for b in range(0, 1001):
+        c = 1000 - a - b
+        if a**2 + b**2 == c**2:
+            print("a, b, c: %d, %d, %d" % (a, b, c))
+end_time = time.time()
+
+print('time: %d' % (end_time - start_time))
